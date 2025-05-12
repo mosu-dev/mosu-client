69c3479eb866db15a24159e35d4eb52b197cd019
toothlessdev <toothless042@gmail.com>
2025-05-13 04:55:10 +0900

    refactor: InputFieldSet 컴포넌트를 사용하여 리팩토링

--- src/pages/auth/member/signin.tsx
+++ src/pages/auth/member/signin.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 import { MosuLogo } from "@/apps/ui/MosuLogo";
 
 import logoKakao from "@/entities/auth/assets/logo-kakao.png";
 
+import { AuthSupportMenu } from "@/widgets/auth/AuthSupportMenu";
+
+import { InputFieldSet } from "@/shared/components/Input";
 import { Button } from "@/shared/ui/button";
-import { Label } from "@/shared/ui/label";
 
 export default function MemberSignInPage() {
     const router = useRouter();
@@ -20,41 +21,22 @@ export default function MemberSignInPage() {
 
             <div>
                 <div className="flex flex-col gap-4">
-                    <div>
-                        <Label htmlFor="email">아이디</Label>
-                        <input
-                            id="email"
-                            type="email"
-                            placeholder="이메일 주소를 입력해주세요"
-                            className="w-full border-b-[#ebebeb] border-b-[1px] h-[52px] focus:outline-none"
-                        />
-                    </div>
-                    <div>
-                        <Label htmlFor="password">비밀번호</Label>
-                        <input
-                            id="password"
-                            type="password"
-                            placeholder="비밀번호를 입력해주세요. (영문, 숫자, 특수문자 조합 8-20자)"
-                            className="w-full border-b-[#ebebeb] border-b-[1px] h-[52px] focus:outline-none"
-                        />
-                    </div>
+                    <InputFieldSet
+                        id="email"
+                        label="아이디"
+                        placeholder="이메일 주소를 입력해주세요"
+                    />
+                    <InputFieldSet
+                        id="password"
+                        label="비밀번호"
+                        type="password"
+                        placeholder="비밀번호를 입력해주세요. (영문, 숫자, 특수문자 조합 8-20자)"
+                    />
                 </div>
 
                 <Button className="w-full h-[60px] mt-8">로그인</Button>
 
-                <ul className="flex justify-center mt-8 h-[20px] text-[#909090]">
-                    <li className="flex-1 text-center">
-                        <Link href="/auth/signup">회원가입</Link>
-                    </li>
-                    <li className="h-full border-r-[1px] border-[#909090]" />
-                    <li className="flex-1 text-center">
-                        <Link href="/auth/signup">아이디 찾기</Link>
-                    </li>
-                    <li className="h-full border-r-[1px] border-[#909090]" />
-                    <li className="flex-1 text-center">
-                        <Link href="/auth/signup">비밀번호 찾기</Link>
-                    </li>
-                </ul>
+                <AuthSupportMenu />
 
                 <div className="flex flex-col gap-2.5 my-14 text-lg">
                     <Button className="w-full h-[60px] bg-[#FEE500] hover:bg-[#FEE500] hover:cursor-pointer  font-semibold text-black text-base">
@@ -67,7 +49,6 @@ export default function MemberSignInPage() {
                         />
                         카카오 로그인
                     </Button>
-
                     <Button
                         variant="outline"
                         className="border-black w-full h-[60px] font-semibold text-black text-base"
--- src/pages/auth/nonmember/signin.tsx
+++ src/pages/auth/nonmember/signin.tsx
@@ -1,7 +1,7 @@
 import { MosuLogo } from "@/apps/ui/MosuLogo";
 
+import { InputFieldSet } from "@/shared/components/Input";
 import { Button } from "@/shared/ui/button";
-import { Label } from "@/shared/ui/label";
 
 export default function NonMemberSignInPage() {
     return (
@@ -12,24 +12,18 @@ export default function NonMemberSignInPage() {
 
             <div>
                 <div className="flex flex-col gap-4">
-                    <div>
-                        <Label htmlFor="student_id">수험번호</Label>
-                        <input
-                            id="student_id"
-                            type="text"
-                            placeholder="수험번호를 입력해주세요."
-                            className="w-full border-b-[#ebebeb] border-b-[1px] h-[52px] focus:outline-none"
-                        />
-                    </div>
-                    <div>
-                        <Label htmlFor="password">비밀번호</Label>
-                        <input
-                            id="password"
-                            type="password"
-                            placeholder="비밀번호를 입력해주세요. (영문, 숫자, 특수문자 조합 8-20자)"
-                            className="w-full border-b-[#ebebeb] border-b-[1px] h-[52px] focus:outline-none"
-                        />
-                    </div>
+                    <InputFieldSet
+                        id="student_id"
+                        label="수험번호"
+                        type="text"
+                        placeholder="수험번호를 입력해주세요."
+                    />
+                    <InputFieldSet
+                        id="password"
+                        label="비밀번호"
+                        type="password"
+                        placeholder="비밀번호를 입력해주세요. (영문, 숫자, 특수문자 조합 8-20자)"
+                    />
                 </div>
 
                 <Button className="w-full h-[60px] mt-8">로그인</Button>
