f6a81edd05ea95a0c38f05c023c42b407cce2931
toothlessdev <toothless042@gmail.com>
2025-05-13 05:08:57 +0900

    refactor: Input, InputFieldSet 컴포넌트 수정 및 InputWithButtonFieldSet 컴포넌트 구현

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,31 +1,60 @@
+import { Fragment } from "react";
+
+import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 
-interface InputProps extends React.ComponentProps<"input"> {
-    sideButton?: React.ReactNode;
-}
+type InputProps = React.ComponentProps<"input">;
 
-export const Input = ({ sideButton, ...props }: InputProps) => {
+export const Input = ({ ...props }: InputProps) => {
     return (
-        <div>
-            <input
-                {...props}
-                className="w-full border-b-[#ebebeb] border-b-[1px] h-[52px] focus:outline-none"
-            ></input>
-            {sideButton}
-        </div>
+        <input
+            {...props}
+            className="w-full border-b-[#ebebeb] border-b-[1px] h-[52px] focus:outline-none"
+        ></input>
     );
 };
 
 interface InputFieldSetProps extends InputProps {
     id: string;
     label: string;
+    required?: boolean;
 }
 
-export const InputFieldSet = ({ id, label, ...props }: InputFieldSetProps) => {
+export const InputFieldSet = ({ id, label, required = false, ...props }: InputFieldSetProps) => {
     return (
         <fieldset>
-            <Label htmlFor={id}>{label}</Label>
+            <Label htmlFor={id} required={required}>
+                {label}
+            </Label>
             <Input id={id} {...props} />
         </fieldset>
+    );
+};
+
+interface InputWithButtonFieldSet extends InputFieldSetProps {
+    buttonLabel: string;
+    buttonOnClick: () => void;
+}
+
+export const InputWithButtonFieldSet = ({
+    id,
+    required,
+    label,
+    buttonLabel,
+    buttonOnClick,
+    ...props
+}: InputWithButtonFieldSet) => {
+    return (
+        <Fragment>
+            <Label htmlFor={id} required={required}>
+                {label}
+            </Label>
+            <div className="flex items-center relative">
+                <Input id={id} {...props} />
+                <Button variant="outline" className="absolute right-0" onClick={buttonOnClick}>
+                    {buttonLabel}
+                </Button>
+            </div>
+        </Fragment>
     );
 };
