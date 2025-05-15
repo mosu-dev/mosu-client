43be128323a731f1c439fc4063cba1962ee17903
toothlessdev <toothless042@gmail.com>
2025-05-15 17:29:45 +0900

    feature: 내 문의글 조회 페이지 구현

--- src/pages/notice/my.tsx
+++ src/pages/notice/my.tsx
@@ -1,7 +1,30 @@
+import { useRouter } from "next/router";
+
+import { PostTable } from "@/entities/posts/ui/PostTable";
+
 import { NoticeLayout } from ".";
 
 export default function NoticeMyQuestionPage() {
-    return <div>my</div>;
+    const router = useRouter();
+
+    return (
+        <div>
+            <PostTable
+                rows={[
+                    {
+                        type: "POST",
+                        id: 1,
+                        title: "재학 중인 학교가 검색이 안됩니다.",
+                        author: "홍*동",
+                        createdAt: "2025-04-26",
+                    },
+                ]}
+                injectOnClickEventHandler={(row) => () => {
+                    router.push(`/notice/announcement/${row.id}`);
+                }}
+            />
+        </div>
+    );
 }
 
 NoticeMyQuestionPage.layout = NoticeLayout;
